81b5b8baa0c920b104729709d9e9637ba35ff0ad
hendisantika <hendisantika@yahoo.co.id>
2019-03-10 06:12:46 +0700

    #2 - Add contract-service as new module

---
+++ contract-service/src/main/java/com/hendisantika/blockchain/contractservice/ContractServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.blockchain.contractservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractServiceApplication.class, args);
+    }
+
+}
